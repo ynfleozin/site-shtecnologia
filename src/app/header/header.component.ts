2aa3c76474d0cbf2038ad073aba76f288867f1c5
Leonardo Alvarenga <leonardoalvarengam@gmail.com>
2024-09-23 18:52:31 -0300

    add responsivity

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
+  constructor(private renderer: Renderer2) {}
 
+  ngAfterViewInit() {
+    const mobileMenuButton = this.renderer.selectRootElement('#mobile-menu-button', true);
+    const mobileMenuList = this.renderer.selectRootElement('#mobile-menu-list', true);
+    const mobileMenuLinks = mobileMenuList.querySelectorAll('a');
+
+    this.renderer.listen(mobileMenuButton, 'click', () => {
+      if (mobileMenuList.classList.contains('show')) {
+        this.renderer.removeClass(mobileMenuList, 'show');
+      } else {
+        this.renderer.addClass(mobileMenuList, 'show');
+      }
+    });
+
+    mobileMenuLinks.forEach((link: Element) => {
+      this.renderer.listen(link, 'click', () => {
+        this.renderer.removeClass(mobileMenuList, 'show');
+      });
+    });
+  }
 }
