11cad52b0f3c0531d48e72672aacd2dd978ea352
ynfleozin <leonardoalvarengam@gmail.com>
2024-10-17 14:28:00 -0300

    convert images .jpg and .png to .webp

--- src/app/cases/cases.component.ts
+++ src/app/cases/cases.component.ts
@@ -7,14 +7,14 @@ import { Component } from '@angular/core';
 })
 export class CasesComponent {
   cases = [
-    { name: 'Microsoft Partner', logo: 'assets/images/cases/microsoft-partner.png' },
-    { name: 'Acronis', logo: 'assets/images/cases/acronis.png' },
-    { name: 'Bit Defender', logo: 'assets/images/cases/bitdefender.png' },
-    { name: 'Opsense', logo: 'assets/images/cases/opnsense.png' },
-    { name: 'Microsoft Azure', logo: 'assets/images/cases/microsoft-azure.png' },
-    { name: 'Google', logo: 'assets/images/cases/google.png' },
-    { name: 'Zoho', logo: 'assets/images/cases/zoho.png' },
-    { name: 'Addee', logo: 'assets/images/cases/addee.png' },
-    { name: 'Starti', logo: 'assets/images/cases/starti.png' },
+    { name: 'Microsoft Partner', logo: 'assets/images/cases/microsoft-partner-logo.webp' },
+    { name: 'Acronis', logo: 'assets/images/cases/acronis.webp' },
+    { name: 'Bit Defender', logo: 'assets/images/cases/bitdefender.webp' },
+    { name: 'Opsense', logo: 'assets/images/cases/opnsense.webp' },
+    { name: 'Microsoft Azure', logo: 'assets/images/cases/microsoft-azure.webp' },
+    { name: 'Google', logo: 'assets/images/cases/google.webp' },
+    { name: 'Zoho', logo: 'assets/images/cases/zoho.webp' },
+    { name: 'Addee', logo: 'assets/images/cases/addee.webp' },
+    { name: 'Starti', logo: 'assets/images/cases/starti.webp' },
   ];
 }
