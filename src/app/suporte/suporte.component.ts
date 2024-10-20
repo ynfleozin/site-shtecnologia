a1213e7736dc2b492a4d0bf530e82b644a884af8
ynfleozin <leonardoalvarengam@gmail.com>
2024-10-20 18:28:35 -0300

    adding chat only in the support component

--- src/app/suporte/suporte.component.ts
+++ src/app/suporte/suporte.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-suporte',
   templateUrl: './suporte.component.html',
-  styleUrl: './suporte.component.css'
+  styleUrls: ['./suporte.component.scss']
 })
-export class SuporteComponent {
-}
+export class SuporteComponent implements OnInit, OnDestroy {
 
- 
+  constructor() { }
+
+  ngOnInit(): void {
+    // Verifica se o script já foi adicionado
+    if (!document.getElementById('chat-script')) {
+      const script = document.createElement('script');
+      script.id = 'chat-script'; // Define um id para evitar múltiplas inserções
+      script.type = 'text/javascript';
+      script.src = 'https://public-assets.tiflux.com/chat_widget.js?organization_token=374d444dffceea5e74efa25e6f70300a808655ed';
+      document.head.appendChild(script);
+
+      // Adiciona as configurações do chat
+      script.onload = () => {
+        (window as any).organization_token = "374d444dffceea5e74efa25e6f70300a808655ed";
+        (window as any).icon_color = "#0cc4b6";
+        (window as any).init_minimal = true;
+        (window as any).show_chat_icon = true;
+      };
+    }
+  }
+
+  ngOnDestroy(): void {
+    // Remove o script de chat ao sair da página
+    const script = document.getElementById('chat-script');
+    if (script) {
+      script.remove();
+    }
+
+    // Remove também o elemento do chat caso ele já tenha sido inserido
+    const chatWidget = document.querySelector('.chat-widget');
+    if (chatWidget) {
+      chatWidget.remove();
+    }
+  }
+}
